b2e2fa49edad9efc4624ab5089ec3167bcae209b
Alina <a.y.kositsyna@gmail.com>
2023-12-26 22:05:20 +0700

    (feat)[Employee]: added string Name, double BaseSalary, IBankService BankService, constructor, virtual string GetInfo, double CalculateSalary

--- program/Employee.cs
+++ program/Employee.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace program
 {
-    internal class Employee
+    abstract class Employee
     {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public IBankService BankService { get; set; }
+
+        public Employee(string name, double salary)
+        {
+            this.Name = name;
+            this.BaseSalary = salary;
+        }
+
+        public virtual string GetInfo()
+        {
+            string result = "Name: " + Name + "\n" +
+                "Salary: " + BaseSalary + "\n" +
+                "Bank: " + BankService + "\n" +
+                "Salary after banking service: " + CalculateSalary(BaseSalary) + "\n";
+            return result;
+        }
+
+        private double CalculateSalary(double salary)
+        {
+            return BankService.CalculateSalary(BaseSalary);
+        }
     }
 }
